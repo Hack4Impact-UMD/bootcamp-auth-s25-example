704570dd2c78dc7aa4aa65a3d2e981193f62b1c4
Gavin <gcrisolo@terpmail.umd.edu>
2025-04-13 23:23:59 -0400

    final touches

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -9,12 +9,12 @@ import { Link, useNavigate } from "react-router";
 export default function AuthPage() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [isSignup, setIsSignup] = useState(false);
+  const [signup, setSignup] = useState(false);
   const navigate = useNavigate();
 
   const handleAuth = async () => {
     try {
-      if (isSignup) {
+      if (signup) {
         const userCred = await createUserWithEmailAndPassword(auth, email, password);
         const defaultData: UserData = { 
           email: email, 
@@ -34,12 +34,12 @@ export default function AuthPage() {
   return (
     <div className="min-h-screen flex items-center justify-center">
       <div className="flex flex-col justify-center items-center w-xl h-100 px-6 py-12 lg:px-8 max-w-lg border-solid border-2 rounded-lg">
-        <h1>{isSignup ? "Sign Up" : "Log In"}</h1>
+        <h1>{signup ? "Sign Up" : "Log In"}</h1>
         <input className="px-3 py-2 w-3/4 mt-5 border rounded focus:ring-2 focus:ring-blue-500" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
         <input className="px-3 py-2 w-3/4 my-3 border rounded focus:ring-2 focus:ring-blue-500" placeholder="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md cursor-pointer" onClick={handleAuth}>{isSignup ? "Sign Up" : "Log In"}</button>
-        <p className="mt-3 text-blue-600 underline cursor-pointer hover:text-blue-800 select-none" onClick={() => setIsSignup(!isSignup)}>
-          {isSignup ? "Already have an account?" : "Need to sign up?"}
+        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-md cursor-pointer" onClick={handleAuth}>{signup ? "Sign Up" : "Log In"}</button>
+        <p className="mt-3 text-blue-600 underline cursor-pointer hover:text-blue-800 select-none" onClick={() => setSignup(!signup)}>
+          {signup ? "Already have an account?" : "Need to sign up?"}
         </p>
         <Link to="/forgot-password" className="text-blue-600 underline cursor-pointer hover:text-blue-800">Forgot password?</Link>
       </div>
